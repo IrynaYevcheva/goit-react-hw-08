9b1d78f8839e6a124ecf1d570793c0be6939b1d1
Iryna Yevcheva <irenmikos@gmail.com>
2024-04-05 23:59:04 +0300

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import styles from './Home.module.css';
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Welcome to PhoneBook</h1>
-      <p>Be sure of the reliability of your contacts</p>
+      <h1 className={styles.title}>Welcome to PhoneBook</h1>
+      <p className={styles.text}>Be sure of the reliability of your contacts</p>
     </div>
   );
 }
